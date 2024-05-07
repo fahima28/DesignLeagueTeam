6fcf5ec679d7f583883ff5119934f993eeb9e99d
Fahim A <147541831+fahima28@users.noreply.github.com>
2024-05-07 13:44:38 -0400

    Update Fahim.js

--- Fahim/Fahim.js
+++ Fahim/Fahim.js
@@ -1,14 +1,3 @@
-//Loader
-window.addEventListener('load'. () => {
-  const loader = document.querySelector('.loader');
-setTimeout(() => {
-  loader.setAttribute('style', 'opacity: 0;');
-}, 500);
-setTimeout(() => {
-  loader.parentNode.removeChild(loader);
-  document.body.style.overflow = 'initial';
-}, 1000);
-});
 
 const coinSfx = new Sfx('sfx/Mario Coin.wav');
 const pipeSfx = new Sfx('sfx/Mario Pipe.wav');
