941835f6c3f8a2c6a6da93ec890ca0da560fb696
Rafael perez <144722762+Rafaelp80@users.noreply.github.com>
2024-05-07 13:34:53 -0400

    Update Rafael.js

--- Rafael/Rafael.js
+++ Rafael/Rafael.js
@@ -19,7 +19,7 @@ let ninja;
 function Sfx(src){
   this.sound = document.createElement('audio');
   this.sound.src = src;
-  this.sound.setAttribute('preload', 'auto);
+  this.sound.setAttribute('preload', 'auto');
   this.sound.style.display = 'none';
   document.body.appendChild(this.sound);
   this.play = function(){
